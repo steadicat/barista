e5fd86d320f6a6fdadd13da27831478ca9e873d3
Stefano J. Attardi <stefano@attardi.org>
2010-05-12 15:26:16 -0400

    Added sample site with tutorial.

---
+++ static/config.rb
@@ -0,0 +1,6 @@
+http_path = "/"
+css_dir = "styles"
+sass_dir = "styles"
+images_dir = "."
+javascripts_dir = "scripts"
+sass_options = { :style => :compressed }
---
+++ static/scripts/main.js
@@ -0,0 +1,5 @@
+//! sprocketize main.js > master.js.tmp && mv master.js.tmp master.js
+
+//= require "barista/barista"
+
+B.start();
